ebb9344ffbcd2bc5e871dbe87af6442982323499
dankito <git@dankito.net>
2025-07-26 17:25:07 +0200

    Added documentation from Loki docs

--- loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/api/LokiApiClient.kt
+++ loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/api/LokiApiClient.kt
@@ -14,7 +14,24 @@ open class LokiApiClient(
     }
 
 
-    open suspend fun queryLabels(query: String? = null, since: String? = null): WebClientResult<LabelsResponse> {
+    /**
+     * Retrieves the list of known labels within a given time span.
+     * Loki may use a larger time span than the one specified.
+     */
+    open suspend fun queryLabels(
+        /**
+         * Log stream selector that selects the streams to match and return label names.
+         * Example: `{app="myapp", environment="dev"}`.
+         * In our implementation the curly braces can be omitted.
+         */
+        query: String? = null,
+        /**
+         * A `duration` used to calculate `start` relative to `end`.
+         * If `end` is in the future, `start` is calculated as this duration before now.
+         * Any value specified for `start` supersedes this parameter.
+         */
+        since: String? = null,
+    ): WebClientResult<LabelsResponse> {
         val queryParams = buildMap {
             if (query != null) { put("query", assertQueryFormat(query)) }
             if (since != null) { put("since", since) }
