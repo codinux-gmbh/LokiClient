610ef29834e922acac32526f5647b7d63bf2c8bb
dankito <git@dankito.net>
2025-07-31 11:56:04 +0200

    Made Instant.minusSeconds() more correct by extracting Instant.toEpochSecondsAsDouble()

--- loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/extensions/InstantExtensions.kt
+++ loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/extensions/InstantExtensions.kt
@@ -4,8 +4,11 @@ import net.dankito.datetime.Instant
 
 const val ThirtyDaysSeconds = 30 * 24 * 60 * 60L
 
+fun Instant.toEpochSecondsAsDouble(): Double =
+    "${epochSeconds}.${nanosecondsOfSecond.toString().padStart(9, '0')}".toDouble()
+
 fun Instant.minusSeconds(seconds: Long): Instant =
-    Instant.ofEpochSeconds(this.epochSeconds.toDouble() - seconds)
+    Instant.ofEpochSeconds(this.toEpochSecondsAsDouble() - seconds)
 
 fun Instant.minusThirtyDays() =
     this.minusSeconds(ThirtyDaysSeconds)
