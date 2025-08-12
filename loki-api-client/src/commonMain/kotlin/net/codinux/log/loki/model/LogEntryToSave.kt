32855b29d799d4f0ccbf426b68ccf20e229a5074
dankito <git@dankito.net>
2025-08-12 04:09:51 +0200

    Set that labels are optional

--- loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/model/LogEntryToSave.kt
+++ loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/model/LogEntryToSave.kt
@@ -3,6 +3,6 @@ package net.codinux.log.loki.model
 data class LogEntryToSave(
     val timestamp: LokiTimestamp,
     val message: String,
-    val labels: Map<String, String>,
+    val labels: Map<String, String> = emptyMap(),
     val structuredMetadata: Map<String, String> = emptyMap(),
 )
