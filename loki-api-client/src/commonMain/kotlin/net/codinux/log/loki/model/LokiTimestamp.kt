817e2acfcdc4314338e55899e1fcf08b13aeb289
dankito <git@dankito.net>
2025-08-12 04:06:06 +0200

    Added alternative ways to create a LokiTimestamp

--- loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/model/LokiTimestamp.kt
+++ loki-api-client/src/commonMain/kotlin/net/codinux/log/loki/model/LokiTimestamp.kt
@@ -1,6 +1,9 @@
 package net.codinux.log.loki.model
 
 import net.dankito.datetime.Instant
+import net.dankito.datetime.LocalDate
+import net.dankito.datetime.LocalDateTime
+import net.dankito.datetime.Month
 import kotlin.jvm.JvmInline
 
 @JvmInline
@@ -9,6 +12,8 @@ value class LokiTimestamp(
 ) {
 
     companion object {
+
+        fun now() = LokiTimestamp(Instant.now())
 
         fun ofEpochSeconds(secondsSinceEpoch: Long) = LokiTimestamp(Instant(secondsSinceEpoch))
 
@@ -19,6 +24,20 @@ value class LokiTimestamp(
         fun ofEpochNanos(nanosecondsSinceEpoch: Long) = LokiTimestamp(Instant.ofEpochNanoseconds(nanosecondsSinceEpoch))
 
         fun ofInstant(instant: Instant) = LokiTimestamp(instant)
+
+        fun ofDate(year: Int, month: Month, day: Int) = ofDate(LocalDate(year, month, day))
+
+        fun ofDate(year: Int, month: Int, day: Int) = ofDate(LocalDate(year, month, day))
+
+        fun ofDate(date: LocalDate) = ofDateTime(date.atStartOfDay())
+
+        fun ofDateTime(year: Int, month: Month, day: Int, hour: Int, minute: Int = 0, second: Int = 0, nanosecondOfSecond: Int = 0) =
+            ofDateTime(LocalDateTime(year, month, day, hour, minute, second, nanosecondOfSecond))
+
+        fun ofDateTime(year: Int, month: Int, day: Int, hour: Int, minute: Int = 0, second: Int = 0, nanosecondOfSecond: Int = 0) =
+            ofDateTime(LocalDateTime(year, month, day, hour, minute, second, nanosecondOfSecond))
+
+        fun ofDateTime(dateTime: LocalDateTime) = LokiTimestamp(dateTime.toInstantAtUtc())
 
     }
 
